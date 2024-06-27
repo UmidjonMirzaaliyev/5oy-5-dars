4d2979144b2d515ad55096b67bee11a49b75f361
UmidjonMirzaaliyev <mirzaaliyev.umidjon07@gmail.com>
2024-06-27 12:22:19 +0500

    yangiland

--- js/script.js
+++ js/script.js
@@ -1,9 +1,38 @@
 const toggle = document.getElementById("mode-toggle");
 toggle.addEventListener("change", () => {
   document.body.classList.toggle("dark-mode");
+  
 });
 
-// Add dark mode styles
+const rightSide = document.querySelector('.rightSide')
+const leftSide = document.querySelector('.leftSide')
+
+const uzbek = document.querySelector('#uzbekLang')
+const eng = document.querySelector('#engLang')
+const rusLang = document.querySelector('#rusLang')
+
+const textChange1 = document.querySelector('.text_change1')
+const textChange2 = document.querySelector('.text_change2')
+const textChange3 = document.querySelector('.text_change3')
+
+uzbek.addEventListener("click", function (){
+  textChange1.textContent = "Figma bilan ijodkorlik kuchini oching!"
+  textChange2.textContent = "Dizayn o'yiningizni oshirishga tayyormisiz?"
+  textChange3.textContent = "Endi Figma-ni sinab ko'ring"
+})
+
+eng.addEventListener("click", function (){
+  textChange1.textContent = "Unlock the Power of Creativity with Figma!"
+  textChange2.textContent = "Ready to elevate your design game?"
+  textChange3.textContent = "Try Figma now"
+})
+
+rusLang.addEventListener("click", function (){
+  textChange1.textContent = "Раскройте силу творчества с Figma!"
+  textChange2.textContent = "Готовы улучшить свою дизайнерскую игру?"
+  textChange3.textContent = "Попробуйте Фигму прямо сейчас"
+})
+
 const css = `
 .dark-mode {
     background-color: #333;
@@ -13,6 +42,8 @@ const css = `
 .dark-mode .left-side {
     background-color: #000;
 }
+
+
 `;
 const style = document.createElement("style");
 style.appendChild(document.createTextNode(css));
